ce755252c08331fc55b3483112b033e7b42f4151
nittin sharma <nittan.sharma@cardinsider.com>
2022-10-08 14:19:11 +0530

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-//import 'package:marpp_web/components/header.dart';
 
 import 'homepage.dart';
 
@@ -11,16 +10,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'marpp_web',
+      title: 'Devfest jammu 2022',
       theme: ThemeData(
         fontFamily: 'Rubik',
         primaryColor: const Color(0XFF885AF8),
       ),
-      home: const MyHomePage(title: 'marpp web application'),
+      home: const MyHomePage(title: 'Devfest 2022 jammu'),
     );
   }
 }
