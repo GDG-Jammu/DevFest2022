42137d396cff630efb9d4d378fe072a2dc613fb1
prashanttyson <prashantmishra1995@gmail.com>
2022-10-10 00:24:38 +0530

    flutter web

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,11 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import 'views/landing_page/desktop_landing.dart';
 import 'views/landing_page/mobile_landing.dart';
 import 'views/landing_page/tablet_landing.dart';
 import 'widgets/responsive_layout.dart';
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -18,13 +19,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.white,
-      body: ResponsiveLayout(
-        mobile: LandingPageMobileView(),
-        tablet: LandingTabletView(),
-        desktop: LandingDesktopView(),
-        //desktop: PricingDesktopView(),
-        //desktop: DeskTopHeader()
-      ),
+      body: LandingDesktopView(),
+      // ResponsiveWidget(
+      //   smallScreen: LandingPageMobileView(),
+      //   mediumScreen: LandingTabletView(),
+      //   largeScreen: LandingDesktopView(),
+      // ),
     );
   }
 }
