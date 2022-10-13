1f9ea752fa52beb81be709c5c3a24a69c0c9e319
prashanttyson <prashantmishra1995@gmail.com>
2022-10-14 01:25:13 +0530

    home

--- lib/widgets/responsive_layout.dart
+++ lib/widgets/responsive_layout.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gdg_devfest/views/landing_page/desktop_landing.dart';
+import 'package:gdg_devfest/views/desktop/desktop_landing.dart';
 
 class ResponsiveWidget extends StatelessWidget {
   final Widget largeScreen;
